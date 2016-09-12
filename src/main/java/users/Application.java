eb260bef7ca1fd38d9afe578abf3e71dcf5f9d4a
petro1 <tancopetronel@yahoo.com>
2016-09-12 12:26:51 +0300

    Update Application.java

--- src/main/java/users/Application.java
+++ src/main/java/users/Application.java
@@ -2,6 +2,7 @@ package users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
 
 	/**
